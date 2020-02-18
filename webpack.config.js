9fd83910efaebc87350bb4aeeb55f97d8ea84ec9
igor pieruccini <igorpieruccini@gmail.com>
2020-02-18 22:48:25 +0100

    chore:  initial project setup
    install all required modules, within basic directories and files to run react on browser.
    
    npm start to start webpack-dev-server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js']
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+};
