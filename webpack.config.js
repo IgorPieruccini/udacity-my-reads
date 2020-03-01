26154ef0071774908d5784fccf8677b3508c5b37
igor pieruccini <igorpieruccini@gmail.com>
2020-03-01 23:40:00 +0100

    chore: update build process
    add assets path
    add build command
    add dist to gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,43 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.jsx',
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader'
+module.exports = env => {
+  return {
+    entry: './src/index.jsx',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: env.ASSET_PATH
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
           }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  resolve: {
-    extensions: ['.jsx', '.js']
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader'
+            }
+          ]
+        }
+      ]
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    resolve: {
+      extensions: ['.jsx', '.js']
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })]
+  };
 };
